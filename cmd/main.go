eb777c6777e55bb505bd6094dfd27dc213833a60
OrlandoRomo <orlando.romo@improving.com>
2021-06-22 23:36:28 -0500

    update main.go file

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 
 func InitCMD() {
 	currentOS := macos.GetInfo()
-	fmt.Printf("\tWelcome ~ %s@%s\n\n", color.RedString(currentOS.Name), color.RedString(currentOS.Host))
+	fmt.Printf("\nWelcome ~ %s@%s\n\n", color.RedString(currentOS.Name), color.RedString(currentOS.Host))
 	fmt.Printf("\t%s %s %s\n", color.RedString("os"), "~", currentOS.OS)
 	fmt.Printf("\t%s %s %s\n", color.GreenString("host"), "~", currentOS.Host)
 	fmt.Printf("\t%s %s %s\n", color.CyanString("uptime"), "~", currentOS.Uptime)
@@ -20,5 +20,5 @@ func InitCMD() {
 	fmt.Printf("\t%s %s %s\n", color.CyanString("terminal"), "~", currentOS.Terminal)
 	fmt.Printf("\t%s %s %s\n", color.BlueString("cpu"), "~", currentOS.CPU)
 	fmt.Printf("\t%s %s %s\n", color.YellowString("gpu"), "~", currentOS.GPU)
-	fmt.Printf("\t%s %s %s\n", color.RedString("memory"), "~", currentOS.Memory)
+	fmt.Printf("\t%s %s %s\n\n", color.RedString("memory"), "~", currentOS.Memory)
 }
