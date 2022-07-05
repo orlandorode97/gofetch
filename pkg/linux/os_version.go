2c92823777014cb0bb73f970011962de2cbfedb4
Orlando Romo <orlando.romo.97@hotmail.com>
2022-07-04 23:36:46 -0500

    use output of /etc/os-release to get the name and version of the current linux os

--- pkg/linux/os_version.go
+++ pkg/linux/os_version.go
@@ -1,12 +1,29 @@
 package linux
 
-import "strings"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var regexOS *regexp.Regexp
 
 // GetHostname returns the hostname of the linux distro.
 func (l *linux) GetOSVersion() string {
-	output, err := execCommand("uname", "-srm").CombinedOutput()
+	regexOS = regexp.MustCompile(`(NAME|VERSION)=(.+)`)
+	// To get os name and version let's use the follow command: grep -E -i -w 'VERSION|NAME' /etc/os-release.
+	output, err := execCommand("grep", "-E -i -w", "/etc/os-release").CombinedOutput()
 	if err != nil {
 		return "Unknown"
 	}
+
+	osVersion := strings.TrimSuffix(string(output), "\n")
+
+	if !regexOS.MatchString(osVersion) {
+		return "Unknown"
+	}
+
+	matches := regexOS.FindStringSubmatch(osVersion)
+	fmt.Println(matches)
 	return strings.TrimSuffix(string(output), "\n")
 }
