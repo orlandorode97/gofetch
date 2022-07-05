516055efa49b1c29898bd470bcc6c2087eb7c50c
Orlando Romo <34588445+orlandorode97@users.noreply.github.com>
2022-07-05 15:29:18 -0500

    OS name and version (#13)
    * use output of /etc/os-release to get the name and version of the current linux os
    
    * refactor os version by getting the correct os name and version
    
    * update os version tests function

--- pkg/linux/os_version.go
+++ pkg/linux/os_version.go
@@ -1,12 +1,39 @@
 package linux
 
-import "strings"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var regexOS *regexp.Regexp
 
 // GetHostname returns the hostname of the linux distro.
 func (l *linux) GetOSVersion() string {
-	output, err := execCommand("uname", "-srm").CombinedOutput()
+	regexOS = regexp.MustCompile(`[^NAME|VERSION=].+`)
+	cmd := "grep -E -i -w '%s' /etc/os-release"
+	output, err := execCommand("bash", "-c", fmt.Sprintf(cmd, "NAME")).CombinedOutput()
 	if err != nil {
 		return "Unknown"
 	}
-	return strings.TrimSuffix(string(output), "\n")
+	name := match(output)
+
+	output, err = execCommand("bash", "-c", fmt.Sprintf(cmd, "VERSION")).CombinedOutput()
+	if err != nil {
+		return "Unknown"
+	}
+
+	version := match(output)
+
+	return name + " " + version
+}
+
+func match(input []byte) string {
+	output := strings.TrimSuffix(string(input), "\n")
+	if !regexOS.MatchString(output) {
+		return "Unknown"
+	}
+	output = regexOS.FindString(output)
+	output = strings.Trim(output, `"`)
+	return output
 }
